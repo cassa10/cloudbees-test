2abc91cd7c7d38a3af05a4964435ef18a3919db2
cassa10 <cassanojoseluis@gmail.com>
2020-08-02 21:26:56 +0000

    Actualizar 'runTests.sh'

--- runTests.sh
+++ runTests.sh
@@ -4,12 +4,12 @@
 # Run all tests
 
 n=1
-max=$1
+cantidadTests=$1
 
 # loop until $n equals $max
-while [ $n -le $max ]
+while [ $n -le $cantidadTests ]
 do
-	echo "Test [$n/10]"
+	echo "Test [$n/$cantidadTests]"
 	echo "OK"
 	n=$(( n+1 ))	 
 done
